8d238fc6727dcc00a89d7cad59b92cf8a8fc61eb
XL1031 <1051389685@qq.com>
2022-09-30 10:04:56 +0800

    添加决策树基本模型

---
+++ decisionTreeModel.py
@@ -0,0 +1,30 @@
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from naiveBayesian import*
+if __name__ == "__main__":
+    # 读取数据
+    data = getAllGoodsNameOfTheColumnWithClassCode('data/test.xlsx', '二类代码', '货名', 'All')
+    # 划分训练集和测试集
+    trainList,trainLabel,testList,testLabel = dataSetSplit(data)
+    # 数据清理
+    trainAfterClean = dataClean(trainList)
+    testAfterClean = dataClean(testList)
+    # 词频模型
+    count_vector = CountVectorizer()
+    trainVectorMatrix = count_vector.fit_transform(trainAfterClean)
+    # print(count_vector.get_feature_names_out())  # 看到所有文本的关键字
+    # print(count_vector.vocabulary_)  # 文本的关键字和其位置
+    # print(trainVectorMatrix.toarray())  # 词频矩阵的结果
+    # 获取训练集合tf-itf矩阵
+    train_tfidf = TfidfTransformer(use_idf=False).fit_transform(trainVectorMatrix)
+    # print(train_tfidf)
+    # 训练决策树
+    DecisionTree = DecisionTreeClassifier(criterion="entropy", splitter="best", max_depth=None, min_samples_split=2, min_samples_leaf=2)
+    DecisionTree.fit(train_tfidf, trainLabel)
+    # 测试决策树模型
+    testVector = count_vector.transform(testList)  # 得到测试集的词频矩阵
+    # 用transformer.fit_transform(词频矩阵)得到TF权重矩阵
+    test_tfidf = TfidfTransformer(use_idf=False).fit_transform(testVector)
+    print(DecisionTree.score(test_tfidf, testLabel))
